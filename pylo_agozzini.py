bad018658289dbeda9930127496903e4739ac366
Leonardo Almeida Lessa <leonardo.a.lessa@gmail.com>
2021-04-12 14:25:18 -0300

    Gave the two debug functions their own command to call from terminal

--- pylo_agozzini.py
+++ pylo_agozzini.py
@@ -1,61 +1,71 @@
 # -*- coding: utf-8 -*-
 
 from music import Music
+from filters import smooth
 
 import wave
 import sys
 import getopt
 import numpy as np
 
-def debug(inputpath, outputpath):
-    """ Helper function for debugging. """
+def copyCommand(inputpath, outputpath):
+    """ Copies music from inputpath to outputpath. """
     music = Music(inputpath)
-    print(music)
     music.output(outputpath)
     
-def smoothtest(path_in, path_out, windowSize, windowType='hanning', points=100, 
+def smoothCommand(path_in, path_out, windowSize, windowType='hanning', points=100, 
                plot=True, save=True):
     ''' Helper function to speed up the workflow with the smooth filter.
     Gets file from "path_in" and applies smooth(windowSize, windowType)
     May save it in 'path_out', under a new name, or plot the first "points" 
     points.
     '''
-    som = Music(path_in)
-    som.smooth(windowSize, windowType)
-    frames = [int(i) for i in som.frames]
+    music = Music(path_in)
+    music.frames = smooth(music, int(windowSize), windowType) # Need to change this!
+    frames = [int(i) for i in music.frames]
     if plot:
         from matplotlib import pyplot as plt
         plt.plot(frames[:points])
     if save:
-        som.output(path_out[:-4] + f'_smooth_{windowType}_{windowSize}.wav')    
-  
-  
+        music.output(path_out[:-4] + f'_smooth_{windowType}_{windowSize}.wav')    
+    
 # Code below adapted from 
 # https://www.tutorialspoint.com/python/python_command_line_arguments.htm
 def main(argv):
     inputfile = ''
     outputfile = ''
+    helpMessage = 'test.py -i <inputfile> -o <outputfile> <command> [<args>]'
     try:
         opts, args = getopt.getopt(argv, 'hi:o:', ['ifile=','ofile='])
     except getopt.GetoptError:
-        print('test.py -i <inputfile> -o <outputfile>')
+        print(helpMessage)
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('test.py -i <inputfile> -o <outputfile>')
+            print(helpMessage)
             sys.exit()
         elif opt in ('-i', '--ifile'):
             inputfile = arg
         elif opt in ('-o', '--ofile'):
             outputfile = arg
-            
-    # Do something with inputfile and outputfile
+    
+    if not args:
+        print('Pass some command')
+        print(helpMessage)
+        sys.exit(2)
+    
     try:
-        debug(inputfile, outputfile)
-        smoothtest(inputfile, outputfile, windowSize=3)
-        
+        if args[0] == 'copy':
+            copyCommand(inputfile, outputfile)
+        elif args[0] == 'smooth':
+            windowSize = int(args[1])
+            smoothCommand(inputfile, outputfile, windowSize)
+        else:
+            print(f'I do not recognize the command {args[0]}')
+            print(helpMessage)
     except FileNotFoundError:
-        print(f"Input file '{inputfile}' was not found. I am going to pass")
+        print(f"Input file '{inputfile}' was not found")
+        print(helpMessage)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
