d4dc1d41688672aa740b36d2f8c1a484d5700c3e
Leonardo Almeida Lessa <leonardo.a.lessa@gmail.com>
2021-04-12 02:32:59 -0300

    PEP8-fying the code: Docstrings and with statements added.

--- pylo_agozzini.py
+++ pylo_agozzini.py
@@ -5,39 +5,42 @@ Created on Sun Apr 11 20:09:43 2021
 @author: Leonardo A. Lessa & Gabriel Solis
 """
 import wave
-import sys, getopt
+import sys
+import getopt
 
 class Music:
     
-    # Opens .wav file at filepath and saves its parameters and frames
+    """ Opens .wav file at filepath and saves its parameters and frames. """
     def __init__(self, filepath):
-        inputfile = wave.open(filepath,mode='rb')
-        self.params = inputfile.getparams()
-        nframes = self.params[3]
-        self.frames = inputfile.readframes(nframes)
-        inputfile.close()
+        with wave.open(filepath,mode='rb') as inputfile:
+            self.params = inputfile.getparams()
+            nframes = self.params[3]
+            self.frames = inputfile.readframes(nframes)
 
-    # TODO: convolutes the music with a filter function. Need to learn about
-    # audio processing techniques, such as windowing
+    """ TODO: convolutes the music with a filter function. Need to learn about
+    audio processing techniques, such as windowing.
+    """
     def convolve(self, f, windowSize):
         pass
 
-    # Saves another .wav. file at filepath with the parameters and frames of
-    # the object
+    """ Saves another .wav. file at filepath with the parameters and frames of
+    the object.
+    """
     def output(self, filepath):
-        outputfile = wave.open(filepath, 'w')
-        outputfile.setparams(self.params)
-        outputfile.writeframes(self.frames)
-        outputfile.close()
+        with wave.open(filepath, 'w') as outputfile:
+            outputfile.setparams(self.params)
+            outputfile.writeframes(self.frames)
 
-# Helper function for debugging
+""" Helper function for debugging. """
 def debug(inputpath, outputpath):
     music = Music(inputpath)
     print(music.params)
     print(music.frames[:10])
     music.output(outputpath)
     
-# Code below adapted from https://www.tutorialspoint.com/python/python_command_line_arguments.htm
+""" Code below adapted from 
+https://www.tutorialspoint.com/python/python_command_line_arguments.htm
+"""
 def main(argv):
     inputfile = ''
     outputfile = ''
@@ -54,8 +57,12 @@ def main(argv):
             inputfile = arg
         elif opt in ("-o", "--ofile"):
             outputfile = arg
+            
     # Do something with inputfile and outputfile
-    debug(inputfile, outputfile)
+    try:
+        debug(inputfile, outputfile)
+    except FileNotFoundError:
+        print(f"Input file '{inputfile}' was not found. I am going to pass")
 
 if __name__ == "__main__":
    main(sys.argv[1:])
