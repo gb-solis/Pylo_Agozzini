352960e84964146c2edccb22527386a383831f17
gb-solis <gabriel.solis@usp.br>
2021-04-12 19:05:35 -0300

    Changed .frames property
    Changed the Music.frames property, so that it now accepts only integer-valued lists of frames.
    The old property, which returned a bytes-valued list, was turned into a hidden property, Music._frames

--- music.py
+++ music.py
@@ -13,7 +13,8 @@ class Music:
         with wave.open(filepath, mode='rb') as inputfile:
             self.params = inputfile.getparams()
             nframes = self.params.nframes
-            self.frames = inputfile.readframes(nframes)  
+            self._frames = inputfile.readframes(nframes) # hidden property
+            self.duration = self.params.nframes/self.params.framerate # seconds
       
     def output(self, filepath):
         """ Saves another .wav. file at filepath with the parameters and frames
@@ -21,7 +22,7 @@ class Music:
         """
         with wave.open(filepath, 'w') as outputfile:
             outputfile.setparams(self.params)
-            outputfile.writeframes(self.frames)
+            outputfile.writeframes(self._frames)
     
     def __repr__(self):
         return str(self.params)
@@ -31,4 +32,20 @@ class Music:
     
     def __getitem__(self, index):
         return self.frames[index]
-
+    
+    @property
+    def frames(self):
+        # takes byte-like frames and presents them as integers between 0, 255
+        byte_frames = self._frames
+        return [round(f) for f in byte_frames]
+    
+    @frames.setter 
+    def frames(self, int_frames):
+        # takes a list of integer frames and stores them as byte-like frames
+        if any(not isinstance(f, int) for f in int_frames):
+            raise ValueError('The frames must be represented by integers')
+        if max(int_frames) > 255:
+            raise ValueError('The frames must be between 0 and 255')
+        
+        self._frames = bytes(int_frames)
+        self.params.nframes = len(self._frames)
